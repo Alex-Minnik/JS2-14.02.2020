b673abd30a7cb418e3d5b1e79b2e4b059cfa76dd
Alex-Minnik <701728@gmail.com>
2020-02-27 00:33:26 +0500

    Удалил лишнии закомментированные строки

--- project/js/main.js
+++ project/js/main.js
@@ -1,21 +1,6 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 // Переделать в ДЗ
-// let getRequest = (url, cb) => {
-//   let xhr = new XMLHttpRequest();
-//   xhr.open('GET', url, true);
-//   xhr.onreadystatechange = () => {
-//     if (xhr.readyState === 4) {
-//       if (xhr.status !== 200) {
-//         console.log('Error');
-//       } else {
-//         cb(xhr.responseText);
-//       }
-//     }
-//   };
-//   xhr.send();
-// };
-
 let getRequest = (url) => {  
   return new Promise ( (resolve, reject) => {
     let xhr = new XMLHttpRequest();
@@ -30,10 +15,8 @@ let getRequest = (url) => {
       }
     };
   xhr.send();
-  });    
-  
+  });
 };
-
 
 class ProductItem {
   constructor(product, img='https://placehold.it/200x150') {
@@ -98,9 +81,7 @@ class ProductList extends List {
     //       this.goods = [...data];
     //       this.render();
     //     });
-    console.log(this.allProducts);     
-  }
-  
+  }  
   
   // _fetchProducts() {
   //   getRequest(`${API}/catalogData.json`)
